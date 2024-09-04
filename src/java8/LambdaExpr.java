662ae45a7c3d8cd24ebf41f0559252b8a3e5fe13
PramodCGowda <pramodcgwd50@gmail.com>
2024-09-03 22:32:57 -0400

    updated code

--- src/java8/LambdaExpr.java
+++ src/java8/LambdaExpr.java
@@ -8,5 +8,6 @@ public class LambdaExpr {
     public static void main(String[] args) {
         Test obj = (name) -> System.out.println("hello " + name);
         obj.SayHello("Pramod");
+        obj.SayHello("Java");
     }
 }
